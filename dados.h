3fc9a8773490e7db97073fdb12d8c6467227e5dc
PeDuggan <pduggan@itba.edu.ar>
2018-10-21 17:08:13 -0300

    actualizado

--- dados.h
+++ dados.h
@@ -1,7 +1,7 @@
 #ifndef DICE_H
 #define DICE_H
 
-void fullroll (unsigned int tiradas,unsigned int cantdados,unsigned int caras);
+void fullroll (unsigned int tiradas,unsigned int cantdados,unsigned int caras,FILE* output);
 /*La funcion fullroll se encarga de tirar dados y aleatoriamente devolver, a
 * traves del stdout que este inicializado imprimir los resultados y su suma.*/
 #endif
